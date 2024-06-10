e4f06c41f82b639e66c1ba489f4e35250e13a2e0
Harrison <harrylearnscopywriting.com>
2024-06-10 17:40:20 +0100

    api project task 3 correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,13 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+
+print(storage.all())
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
